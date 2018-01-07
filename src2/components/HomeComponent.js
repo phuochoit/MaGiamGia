c0f3a090c3f72802bd9777a652fd7d414f825fd9
Phuochoit <phuocck8@gmail.com>
2018-01-07 22:01:17 +0700

    update new verstion

--- src2/components/HomeComponent.js
+++ src2/components/HomeComponent.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { Text, View } from "react-native";
-import Header_Component from "./header";
 class HomeComponent extends Component {
     state = {  }
     render() {
         return (
             <View>
-                <Header_Component navigation={this.props.navigation} menuleft="0" menuright="0" menutitle="Trang Chủ" />
                 <Text>Home</Text>
             </View>
         );
