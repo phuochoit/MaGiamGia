c8f29cfa1010a6a8f160510c84f8181fc1070722
Phuochoit <phuocck8@gmail.com>
2018-01-22 15:22:20 +0700

    clear code

--- src2/sagas/rootSaga.js
+++
@@ -1,9 +0,0 @@
-// import redux-saga
-import { call, all } from "redux-saga/effects";
-
-// import file
-import { fetchTopProduct } from "./topPorductSagas";
-
-export default function* rootSaga(){
-    yield call(fetchTopProduct);
-}
