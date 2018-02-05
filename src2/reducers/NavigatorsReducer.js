e8e4d96bd3f1b31598ba3f0a77b41353038f7978
Phuochoit <phuocck8@gmail.com>
2018-02-05 16:13:38 +0700

    clear code master

--- src2/reducers/NavigatorsReducer.js
+++
@@ -1,9 +0,0 @@
-import AppNavigator from "../navigators/AppNavigator";
-const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('HOME'));
-
-const NavigatorsReducer = (state = initialState, action) => {
-    const nextState = AppNavigator.router.getStateForAction(action, state);
-
-    return nextState || state;
-};
-export default NavigatorsReducer;
